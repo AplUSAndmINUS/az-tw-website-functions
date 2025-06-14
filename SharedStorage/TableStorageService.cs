27d9d7ea1687fb9dd9f77ae64f2e0be70aa6016f
Terence Waters <12157591+AplUSAndmINUS@users.noreply.github.com>
2025-06-14 13:44:46 -0600

    updated the table storage account service

---
+++ SharedStorage/TableStorageService.cs
@@ -0,0 +1,21 @@
+﻿using Azure.Data.Tables;
+using Azure.Identity;
+using System;
+
+namespace az_tw_website_functions.SharedStorage;
+
+public class TableStorageService
+{
+    private readonly TableServiceClient _tableServiceClient;
+
+    public TableStorageService(string storageAccountName)
+    {
+        var endpoint = $"https://{storageAccountName}.table.core.windows.net";
+        _tableServiceClient = new TableServiceClient(new Uri(endpoint), new DefaultAzureCredential());
+    }
+
+    public TableClient GetTableClient(string tableName)
+    {
+        return _tableServiceClient.GetTableClient(tableName);
+    }
+}
