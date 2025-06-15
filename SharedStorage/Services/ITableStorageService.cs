71715ac27bfb57f4f34716441415947a6569d2b8
Terence Waters <12157591+AplUSAndmINUS@users.noreply.github.com>
2025-06-15 13:56:56 -0600

    fixed some implementation errors of records in table storage

--- SharedStorage/Services/ITableStorageService.cs
+++ SharedStorage/Services/ITableStorageService.cs
@@ -13,7 +13,7 @@ public interface ITableStorageService
 {
     TableClient GetTableClient(string tableName);
     Task<TableEntity?> GetEntityAsync(string tableName, string partitionKey, string rowKey);
-    Task<(IEnumerable<TableEntity> Entities, string? ContinuationToken)> GetEntitiesAsync(string tableName, string? filter = null, int pageSize = 25, string? continuationToken = null);
+    Task<TablePageResult> GetEntitiesAsync(string tableName, string? filter = null, int pageSize = 25, string? continuationToken = null);
     Task UpsertEntityAsync(string tableName, ITableEntity entity);
     Task DeleteEntityAsync(string tableName, string partitionKey, string rowKey);
 }
