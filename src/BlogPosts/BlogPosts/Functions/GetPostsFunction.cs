c3eace2321ba1047ed83eed8c41b6b148978f667
Terence Waters <12157591+AplUSAndmINUS@users.noreply.github.com>
2025-06-16 17:23:54 -0600

    beginning stages of adding models for blog table storage

---
+++ src/BlogPosts/BlogPosts/Functions/GetPostsFunction.cs
@@ -0,0 +1,21 @@
+﻿namespace BlogPosts.Functions;
+
+public class GetAllBlogPosts
+{
+  public GetAllBlogPosts(ILogger<GetAllBlogPosts> logger)
+  {
+      _logger = logger;
+  }
+
+  [Function("GetAllBlogPosts")]
+  public async Task<HttpResponseData> Run(
+      [HttpTrigger(AuthorizationLevel.Function, "get", Route = "blogposts")] HttpRequestData req)
+  {
+      _logger.LogInformation("C# HTTP trigger function processed a request.");
+
+      var response = req.CreateResponse(HttpStatusCode.OK);
+      response.Headers.Add("Content-Type", "application/json");
+      await response.WriteStringAsync("[]");
+      return response;
+  }
+}
