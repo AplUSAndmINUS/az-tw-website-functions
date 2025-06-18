00e7da474910b3dabf8e48055318624874fa419c
Terence Waters <12157591+AplUSAndmINUS@users.noreply.github.com>
2025-06-18 13:44:38 -0600

    created Constants within Utils for Cdn and newly created API URLs in Azure DNS

--- Utils/CdnUrlBuilder.cs
+++ Utils/CdnUrlBuilder.cs
@@ -4,17 +4,6 @@ using Utils.Constants;
 
 public static class CdnUrlBuilder
 {
-  // CDN endpoints for content types
-  private const string CdnEndpointDocuments = "https://documents.terencewaters.com";
-  private const string CdnEndpointImages = "https://images.terencewaters.com";
-  private const string CdnEndpointMusic = "https://music.terencewaters.com";
-  private const string CdnEndpointVideos = "https://videos.terencewaters.com";
-  private const string CdnEndpointMedia = "https://media.terencewaters.com";
-
-  // Mock Azure storage URL which point directly to Azure Blob Storage
-  private const string MockCdnBlobStorageUrl = "https://aztwwebsitestorage.blob.core.windows.net";
-  private const string MockCdnTableStorageUrl = "https://aztwwebsitestorage.table.core.windows.net";
-
   public static string ResolveCdnUrl(ContentSections section, AssetType? assetType, string blobName, string? paramsString = null, bool isMockStorage = false)
   {
     if (string.IsNullOrWhiteSpace(blobName))
@@ -23,7 +12,7 @@ public static class CdnUrlBuilder
       throw new ArgumentException("Blob name cannot be a mock blob.", nameof(blobName));
 
     if (isMockStorage)
-      return $"{MockCdnBlobStorageUrl}/{ContentNameResolver.GetBlobContainerName(section, assetType, true)}/{blobName}";
+      return $"{ApiUrls.MockCdnBlobStorageUrl}/{ContentNameResolver.GetBlobContainerName(section, assetType, true)}/{blobName}";
 
     string containerName = ContentNameResolver.GetBlobContainerName(section, assetType);
 
@@ -41,11 +30,11 @@ public static class CdnUrlBuilder
   {
     return (section, assetType) switch
     {
-      (ContentSections.Documents, _) => $"{CdnEndpointDocuments}/{containerName}/{blobName}",
-      (_, AssetType.Images) => $"{CdnEndpointImages}/{containerName}/{blobName}",
-      (_, AssetType.Video) => $"{CdnEndpointVideos}/{containerName}/{blobName}",
-      (_, AssetType.Media) => $"{CdnEndpointMedia}/{containerName}/{blobName}",
-      (ContentSections.Music, _) => $"{CdnEndpointMusic}/{containerName}/{blobName}",
+      (ContentSections.Documents, _) => $"{ApiUrls.CdnEndpointDocuments}/{containerName}/{blobName}",
+      (_, AssetType.Images) => $"{ApiUrls.CdnEndpointImages}/{containerName}/{blobName}",
+      (_, AssetType.Video) => $"{ApiUrls.CdnEndpointVideos}/{containerName}/{blobName}",
+      (_, AssetType.Media) => $"{ApiUrls.CdnEndpointMedia}/{containerName}/{blobName}",
+      (ContentSections.Music, _) => $"{ApiUrls.CdnEndpointMusic}/{containerName}/{blobName}",
       _ => throw new ArgumentException($"No CDN endpoint configured for section {section} with asset type {assetType}", nameof(section))
     };
   }
